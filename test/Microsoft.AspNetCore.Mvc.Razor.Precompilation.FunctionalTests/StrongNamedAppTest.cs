f05859c45c70b13f25d53b334c30dd72145ade7a
Pranav K <prkrishn@hotmail.com>
2016-09-12 14:56:09 -0700

    Add a test that verifies strong name signing works for precompiled views
    Fixes #16

---
+++ test/Microsoft.AspNetCore.Mvc.Razor.Precompilation.FunctionalTests/StrongNamedAppTest.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.Precompilation
+{
+    public class StrongNamedAppTest : IClassFixture<StrongNamedAppTest.StrongNamedAppFixture>
+    {
+        public StrongNamedAppTest(StrongNamedAppFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        public ApplicationTestFixture Fixture { get; }
+
+        public static IEnumerable<object[]> SupportedFlavorsTheoryData
+        {
+            get
+            {
+                return RuntimeFlavors.SupportedFlavors.Select(f => new object[] { f });
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(SupportedFlavorsTheoryData))]
+        public async Task PrecompiledAssembliesUseSameStrongNameAsApplication(RuntimeFlavor flavor)
+        {
+            // Arrange
+            using (var deployer = Fixture.CreateDeployment(flavor))
+            {
+                var deploymentResult = deployer.Deploy();
+
+                // Act
+                var response = await Fixture.HttpClient.GetStringWithRetryAsync(
+                    deploymentResult.ApplicationBaseUri,
+                    Fixture.Logger);
+
+                // Assert
+                TestEmbeddedResource.AssertContent("StrongNamedApp.Home.Index.txt", response);
+            }
+        }
+
+        public class StrongNamedAppFixture : ApplicationTestFixture
+        {
+            public StrongNamedAppFixture()
+                : base("StrongNamedApp")
+            {
+            }
+        }
+    }
+}
