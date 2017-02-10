1bd74c6a98c00395e4d2b31422f5edbc0d51e4ee
Roderic Bos <git@rooc.nl>
2017-02-10 11:06:39 +0100

    added functional test

---
+++ test/Microsoft.AspNetCore.Mvc.Razor.ViewCompilation.FunctionalTests/SimpleAppWithAssemblyRenameTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.ViewCompilation
+{
+    public class SimpleAppWithAssemblyRenameTest : IClassFixture<SimpleAppWithAssemblyRenameTest.SimpleAppWithAssemblyRenameTestFixture>
+    {
+        public SimpleAppWithAssemblyRenameTest(SimpleAppWithAssemblyRenameTestFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        public ApplicationTestFixture Fixture { get; }
+
+        public static TheoryData SupportedFlavorsTheoryData => RuntimeFlavors.SupportedFlavorsTheoryData;
+
+        [Theory(Skip = "MVC #5736")]
+        [MemberData(nameof(SupportedFlavorsTheoryData))]
+        public async Task Precompilation_WorksForSimpleApps(RuntimeFlavor flavor)
+        {
+            // Arrange
+            using (var deployer = Fixture.CreateDeployment(flavor))
+            {
+                var deploymentResult = deployer.Deploy();
+
+                // Act
+                var response = await Fixture.HttpClient.GetStringWithRetryAsync(
+                    deploymentResult.ApplicationBaseUri,
+                    Fixture.Logger);
+
+                // Assert
+                TestEmbeddedResource.AssertContent("SimpleAppWithAssemblyRenameTest.Home.Index.txt", response);
+            }
+        }
+
+        public class SimpleAppWithAssemblyRenameTestFixture : ApplicationTestFixture
+        {
+            public SimpleAppWithAssemblyRenameTestFixture()
+                : base("SimpleAppWithAssemblyRename")
+            {
+            }
+        }
+    }
+}
