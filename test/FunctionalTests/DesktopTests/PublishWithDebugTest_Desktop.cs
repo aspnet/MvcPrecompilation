418e4c693123a55c11a2a55456694698b60f8ee7
Pranav K <prkrishn@hotmail.com>
2017-11-15 11:03:08 -0800

    Update samples and tests to target netcoreapp2.1

--- test/FunctionalTests/DesktopTests/PublishWithDebugTest_Desktop.cs
+++ test/FunctionalTests/DesktopTests/PublishWithDebugTest_Desktop.cs
@@ -1,18 +1,22 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IntegrationTesting;
+using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.Logging.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace FunctionalTests
 {
-    public class PublishWithDebugTest_CoreCLR :
-        LoggedTest, IClassFixture<PublishWithDebugTest_CoreCLR.TestFixture>
+    [OSSkipCondition(OperatingSystems.Linux)]
+    [OSSkipCondition(OperatingSystems.MacOSX)]
+    public class PublishWithDebugTest_Desktop :
+        LoggedTest, IClassFixture<PublishWithDebugTest_Desktop.TestFixture>
     {
-        public PublishWithDebugTest_CoreCLR(
+        public PublishWithDebugTest_Desktop(
             TestFixture fixture,
             ITestOutputHelper output)
             : base(output)
@@ -22,6 +26,7 @@ namespace FunctionalTests
 
         public ApplicationTestFixture Fixture { get; }
 
+        [ConditionalFact]
         [Fact]
         public async Task PublishingInDebugWorks()
         {
@@ -30,18 +35,18 @@ namespace FunctionalTests
                 // Arrange
                 var deployment = await Fixture.CreateDeploymentAsync(loggerFactory);
 
-                // Act
-                var response = await deployment.HttpClient.GetStringWithRetryAsync(
-                    deployment.ApplicationBaseUri,
-                    loggerFactory.CreateLogger(Fixture.ApplicationName));
-
                 // Assert
-                TestEmbeddedResource.AssertContent("SimpleAppTest.Home.Index.txt", response);
+                Assert.True(File.Exists(Path.Combine(deployment.ContentRoot, $"{Fixture.ApplicationName}.PrecompiledViews.dll")));
             }
         }
 
         public class TestFixture : CoreCLRApplicationTestFixture<SimpleApp.Startup>
         {
+            public TestFixture()
+            {
+                PublishOnly = true;
+            }
+
             protected override DeploymentParameters GetDeploymentParameters()
             {
                 var deploymentParameters = base.GetDeploymentParameters();
